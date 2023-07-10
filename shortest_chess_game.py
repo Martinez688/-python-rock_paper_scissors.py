28fd54f02f154f68ae68f59dca15121e552fbac5
martinez <139108067+Martinez688@users.noreply.github.com>
2023-07-10 09:16:58 +0200

    to shortest_chess_game.py
    # Самая короткая партия в шахматы
    
    Добро пожаловать в программу, которая моделирует самую короткую партию в шахматы, известную как "Мат Лепулле" или "Мат Берлина". В этой партии белые выигрывают за два хода.
    
    ## Как использовать программу
    
    1. Убедитесь, что у вас установлен Python.
    2. Скачайте файл `shortest_chess_game.py` из этого репозитория.
    3. Откройте командную строку (терминал) и перейдите в каталог, где находится файл `shortest_chess_game.py`.
    4. Запустите программу с помощью команды `python shortest_chess_game.py`.
    5. Посмотрите ходы белых и чёрных в самой короткой партии в шахматы.

---
+++ shortest_chess_game.py
@@ -0,0 +1,18 @@
+def shortest_chess_game():
+    print("Самая короткая партия в шахматы: Мат Лепулле")
+
+    # Ходы белых
+    print("Ходы белых:")
+    print("1. f3")
+    print("2. g4")
+
+    # Ходы чёрных
+    print("Ходы чёрных:")
+    print("2... e5")
+    print("1... Qh4#")
+
+def main():
+    shortest_chess_game()
+
+if __name__ == '__main__':
+    main()
